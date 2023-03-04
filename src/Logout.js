1a90a16765602be4b9f9f9eeb374b32d364d2235
potato <jaafariramadan@gmail.com>
2023-03-04 23:28:36 +0200

    Implemented logout

--- src/Logout.js
+++ src/Logout.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import { Navigate } from 'react-router';
 
 import bootstrap from "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import './Logout.css'
 
 class Logout extends React.Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            redirect: ''
+        }
+
+        this.logout = this.logout.bind(this);
+    }
+
+    logout() {
+        localStorage.removeItem('login');
+        this.setState({redirect: <Navigate to='/'/>});
+    }
+
     render() {
         return (
-            <div>
-
+            <div class="col-md-6 text-center">
+                {this.state.redirect}
+                <button class="logout btn btn-secondary" onClick={this.logout}> Logout </button>
             </div>
         );
     }
