725929b68ba012cf7c8ba96f6ca8779710ddcaad
potato <jaafariramadan@gmail.com>
2023-03-04 20:25:52 +0200

    Added Login validate

--- src/Login.js
+++ src/Login.js
@@ -1,15 +1,65 @@
 import React from "react";
 
+import swal from "sweetalert";
+
+import './Login.css';
 
 class Login extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            user: {
+                name: '',
+                email: '',
+                password: ''
+            }
+        }
+    }
+
+    login(e) {
+        e.preventDefault();
+
+        this.setState({user: {
+            email: document.getElementById('email').value,
+            password: document.getElementById('password').value
+        }}, () => {
+            const validate = this.#validateUser();
+            if(validate != 'Valid')
+            {
+                swal(validate, ':(', 'error');
+                return;
+            }
+
+            const auth = this.#authenticate();
+            if(!auth)
+            {
+                swal('Invalid Credintials!', ':(', 'error');
+            }
+        })
+    }
+
     render() {
         return (
-        <form class="col-md-6" action="">
-            <input class="login" type="text" />
-            <input class="login" type="text" />
+        <form class="col-md-6" onSubmit={(e) => this.login(e)}>
+            <input id="email" class="login" type="email" name="email"/>
+            <input id="password" class="login" type="text" name="password"/>
             <button class="login">Login</button>
         </form>
         );
+    }
+
+    #validateUser() {
+        if(!this.state.user.email)
+            return 'Email is required!';
+        if(!this.state.user.password)
+            return 'Password is required!';
+        
+        return 'Valid'
+    }
+
+    #authenticate() {
+        return false;
     }
 }
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 
 import bootstrap from "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import './Navbar.css';
 
 import Login from './Login';
 
